c04bcfbcccc3ade8d436853aa812b7e37f5f5491
mtandy <mtandy@mtandy-desktop>
2016-10-17 00:37:26 +0100

    Do we need % signs?

--- buildlibs.sh
+++ buildlibs.sh
@@ -22,11 +22,7 @@ elif [[ "$UNAME_STR" == "Darwin" ]]; then
   gcc --version > $MAVEN_PROJECTBASEDIR/target/classes/nativelibs/osx-compiler-version.txt
 
 elif [[ "$UNAME_STR" == "MSYS_NT-6.3" ]]; then
-  echo "JAVA_HOME: $JAVA_HOME"
-
-  #cl -o jnicpplib.dll JNICppImplementation.cpp /I D:\jdk1.5.0\include /I D:\jdk1.5.0\include\win32 /link /DLL
-  /C/MinGW/bin/gcc.exe -shared -o "$MAVEN_PROJECTBASEDIR/target/classes/nativelibs/amd64/CrashJvm.dll" -I $JAVA_HOME/include -I $JAVA_HOME/include/win32 "$MAVEN_PROJECTBASEDIR/src/main/java/uk/me/mjt/CrashJvm.c"
-  /C/MinGW/bin/gcc.exe --version > $MAVEN_PROJECTBASEDIR/target/classes/nativelibs/windows-compiler-version.txt
+  echo "Hopefully this is running on appveyor, and the libraries have already been built!"
   
 else
   echo "Didn't recognise $UNAME_STR" >&2
